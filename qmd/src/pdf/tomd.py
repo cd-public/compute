22ce28b47a2a0cb1198e2e7d8d746f8ab5ccccbd
cd <cd>
2025-05-13 22:09:53 -0700

    01 py start

--- qmd/src/pdf/tomd.py
+++
@@ -1,9 +0,0 @@
-from markitdown import MarkItDown
-import glob, os
-
-md = MarkItDown()
-
-for file in glob.glob("*.pdf"):
-    print(file)
-    result = md.convert(file)
-    open(file.replace(".pdf", ".qmd"), "w", encoding="utf-8").write(result.text_content)
